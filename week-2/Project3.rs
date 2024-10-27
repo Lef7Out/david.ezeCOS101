36156447359e3443bca6d6919bb1aa87a52012bb
Lef7Out <daveze727@gmail.com>
2024-10-27 23:59:34 +0000

    (Made final answer round up to 2d.p)

--- week-2/Project3.rs
+++ week-2/Project3.rs
@@ -6,9 +6,9 @@ fn main() {
 
     //Write all equations/functions that will be used
     let a = p*(1.0-(r/100.0)).powf(t);
-    println!("The price of the TV has reduced to {}₦ over 3 years.", a);
+    println!("The price of the TV has reduced to {:.2}₦ over 3 years.", a);
 
     let d = p-a;
-    println!("The price of the TV reduced by {}₦",d);
+    println!("The price of the TV reduced by {:.2}₦",d);
 
 }
