afa778bd9899535daa834e2a08280165066ac967
Lef7Out <daveze727@gmail.com>
2024-10-27 22:56:34 +0000

    (List all parameters)

---
+++ week-2/Project2.rs
@@ -0,0 +1,8 @@
+fn main() {
+//List all parameters
+    let t: f64 = 450000.0 //2
+    let m: f64 = 1500000.0 //1
+    let h: f64 = 750000.0 //3
+    let d: f64 = 2850000.0 //3
+    let a: f64 = 250000.0 //1
+}
