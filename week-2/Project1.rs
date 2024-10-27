cd512d7214ba2b5c30dcc639963a759c41a56656
Lef7Out <daveze727@gmail.com>
2024-10-27 22:37:46 +0000

    (Minor errors fixe

--- week-2/Project1.rs
+++ week-2/Project1.rs
@@ -1,12 +1,12 @@
 fn main() {
     //Write out the parameters for each variable
-    Let p:f64 = 520000000;
-    Let r:f64 = 10%;
-    Let y:f64 = 5;
+    let p: f64 = 520000000.0;
+    let r: f64 = 0.1;
+    let y: f64 = 5.0;
 
-    Let a = p * (1.0 + (r/100)^ y);
+    let a = p * (1.0 + (r / 100.0)).powf(y);
     println!("Amount is {}", a);
-    Let ci = a - p;
+    let ci = a - p;
     println!("Compound Intrest is {}", ci);
 
 
