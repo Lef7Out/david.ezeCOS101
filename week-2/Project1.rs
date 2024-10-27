3421993b8c73d95759a112cecdb2cb6a5c8f2ce1
Lef7Out <daveze727@gmail.com>
2024-10-27 22:44:13 +0000

    (Project 1 Complet minor errors corrected

--- week-2/Project1.rs
+++ week-2/Project1.rs
@@ -1,7 +1,7 @@
 fn main() {
     //Write out the parameters for each variable
     let p: f64 = 520000000.0;
-    let r: f64 = 0.1;
+    let r: f64 = 10.0;
     let y: f64 = 5.0;
 
     let a = p * (1.0 + (r / 100.0)).powf(y);
