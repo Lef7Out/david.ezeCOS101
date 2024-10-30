840cbdf13b7ae750d76b31353fbe92a295d8c7e2
Lef7Out <daveze727@gmail.com>
2024-10-30 15:25:56 +0000

    (Finished editing and debuggind code)

--- week-3/practice2/src/main.rs
+++ week-3/practice2/src/main.rs
@@ -1,21 +1,21 @@
 fn main() {
     // addition
-    Let sum = 5550 + 7310
-    println!("Thehe sum of 5550 and 7310 = {}", sum);
+    let addition: u32 = 5550 + 7310;
+    println!("The sum of 5550 and 7310 = {}", addition);
 
     // addition
-    Let difference:u32 = 95.5 - 4.3;
+    let difference: f32 = 95.5 - 4.3;
     println!("The difference of 95.5 and 4.3 = {}", difference);
 
     //Multiplication
-    Let product:f32 = 4 * 30;
-    println!("The product of 5 and 30= {}", product);
+    let product: i32 = 4 * 30;
+    println!("The product of 5 and 30 = {}", product);
 
     //Division
-    Let quotient = 56.7 / 32.2;
-    println!("the division of 56.7 and 32.2 = {}", quotient);
+    let quotient: f64 = 56.7 / 32.2;
+    println!("The division of 56.7 and 32.2 = {:.3}", quotient);
 
     //remainder
-    Let remainder = 43 % 5
-    println!("the remainder of 43 and 5 = {}", remainder);
+    let remainder  = 43 % 5;
+    println!("The remainder of 43 and 5 = {}", remainder);
 }
