64b351ff8975f8bb2b12fe99183b2a5445c81bca
Lef7Out <daveze727@gmail.com>
2024-11-26 23:03:48 +0100

    Practice 2 Complete

---
+++ week-6/practice2/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+   let empty_string = String::new();
+   println!("Length of empty string: {}", empty_string.len());
+
+   let conent_string = String::new();
+   println!("Content string: {}", conent_string.len());
+}
